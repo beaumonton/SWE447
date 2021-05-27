77e0f35237be4be555bf0e77242731da2c82f1c8
beaumonton <66109136+beaumonton@users.noreply.github.com>
2021-05-27 12:13:47 -0700

    Create cube.jl

---
+++ Assignment-4/cube.jl
@@ -0,0 +1,22 @@
+
+var gl = null;
+
+function init() 
+{
+  var canvas = document.getElementById( "webgl-canvas" );
+  gl = WebGLUtils.setupWebGL( canvas );
+  if ( !gl ) 
+  {
+    alert("Unable to setup WebGL");
+    return;
+  }
+  Cube(gl);
+  render();
+}
+
+function render() 
+{
+    Cube.render;
+}
+
+window.onload = init;
